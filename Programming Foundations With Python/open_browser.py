5724f46d7d07992f3286e84cfeb6692cfa7857a5
Yasin Razlik <yrazlik@ebay.com>
2017-01-23 14:17:38 +0200

    first commit

---
+++ Programming Foundations With Python/open_browser.py
@@ -0,0 +1,9 @@
+import webbrowser
+import time
+
+count = 0;
+while (count < 3):
+    time.sleep(10)
+    webbrowser.open('http://www.youtube.com/watch?v=dQw9WgXcQ')
+    count = count + 1
+
---
+++ Programming Foundations With Python/rename_files.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import os
+def rename_files():
+    #(1) get file names from a folder
+    file_list = os.listdir(r"/Users/yrazlik/Projects/Python/python/files_to_be_renamed/prank")
+    print(file_list)
+    saved_path = os.getcwd();
+    print("Current working directory is " + saved_path)
+    os.chdir(r"/Users/yrazlik/Projects/Python/python/files_to_be_renamed/prank")
+
+                           
+    #(2) for each file, rename filename
+    for file_name in file_list:
+         os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
+
+rename_files()
