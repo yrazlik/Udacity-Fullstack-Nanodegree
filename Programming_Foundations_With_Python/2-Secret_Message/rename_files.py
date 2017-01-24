4dadfa6e7ed700b75817e3b90dd6f0a4ec3a407a
Yasin Razlik <yrazlik@ebay.com>
2017-01-24 10:03:58 +0200

    file io

--- Programming_Foundations_With_Python/2-Secret_Message/rename_files.py
+++ Programming_Foundations_With_Python/2-Secret_Message/rename_files.py
@@ -2,11 +2,11 @@
 import os
 def rename_files():
     #(1) get file names from a folder
-    file_list = os.listdir(r"/Users/yrazlik/Projects/Python/python/files_to_be_renamed/prank")
+    file_list = os.listdir(r"/Users/yrazlik/Projects/Udacity_Fullstack_Nanodegree/Programming_Foundations_With_Python/2-Secret_Message/files_to_be_renamed/prank")
     print(file_list)
     saved_path = os.getcwd();
     print("Current working directory is " + saved_path)
-    os.chdir(r"/Users/yrazlik/Projects/Python/python/files_to_be_renamed/prank")
+    os.chdir(r"/Users/yrazlik/Projects/Udacity_Fullstack_Nanodegree/Programming_Foundations_With_Python/2-Secret_Message/files_to_be_renamed/prank")
 
                            
     #(2) for each file, rename filename
---
+++ Programming_Foundations_With_Python/5-File_IO/file_io.py
@@ -0,0 +1,18 @@
+import urllib
+
+
+def read_text():
+    quotes = open("/Users/yrazlik/Projects/Udacity_Fullstack_Nanodegree/Programming_Foundations_With_Python/5-File_IO/movie_quotes.txt")
+    contents_of_file = quotes.read()
+    print(contents_of_file)
+    quotes.close()
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
+    output = connection.read()
+    print(output)
+    connection.close()
+
+
+read_text()
