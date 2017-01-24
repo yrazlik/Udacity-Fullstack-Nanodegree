16afa9caecd2a911b6ac0826b61aa70ba96a4c08
Yasin Razlik <yrazlik@ebay.com>
2017-01-24 11:10:04 +0200

    Movies project

--- Programming_Foundations_With_Python/5-File_IO/file_io.py
+++ Programming_Foundations_With_Python/5-File_IO/file_io.py
@@ -4,14 +4,20 @@ import urllib
 def read_text():
     quotes = open("/Users/yrazlik/Projects/Udacity_Fullstack_Nanodegree/Programming_Foundations_With_Python/5-File_IO/movie_quotes.txt")
     contents_of_file = quotes.read()
-    print(contents_of_file)
+    # print(contents_of_file)
     quotes.close()
     check_profanity(contents_of_file)
 
 def check_profanity(text_to_check):
     connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text_to_check)
     output = connection.read()
-    print(output)
+
+    # print(output)
+
+    if "true" in output:
+        print("Profanity Alert!!!")
+    elif "false" in output:
+        print("This document has no cursed words!")
     connection.close()
 
 
