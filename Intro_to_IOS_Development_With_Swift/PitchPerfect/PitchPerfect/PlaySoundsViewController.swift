9d2f1e2b499601284cee00c6a93fae18c4793aad
Yasin Razlik <yrazlik@ebay.com>
2017-03-08 13:47:50 +0200

    Finished PitchPerfect

--- Intro_to_IOS_Development_With_Swift/PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
+++ Intro_to_IOS_Development_With_Swift/PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -4,18 +4,39 @@
 //
 //  Created by Razlik, Yasin(AWF) on 06/03/17.
 //  Copyright © 2017 Razlik, Yasin. All rights reserved.
-//
+//audioEngine
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
-    var recordedAudioURL: URL!
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, lowpitch, highpitch, echo, reverb
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("PlaySoundsViewController loaded. The recordedAudioURL is " + recordedAudioURL.absoluteString)
-        // Do any additional setup after loading the view.
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(.notPlaying)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,15 +44,28 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("play button pressed")
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .lowpitch:
+            playSound(pitch: 1000)
+        case .highpitch:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
     }
-    */
-
+    
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+        print("stop button pressed")
+        stopAudio()
+    }
 }
