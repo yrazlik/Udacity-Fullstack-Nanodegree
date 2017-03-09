794967644f16e964acde4b35cd701aa4074026a8
Yasin Razlik <yrazlik@ebay.com>
2017-03-09 10:12:24 +0200

    Started implementing ColorMakerWithSliders

---
+++ IOS_Nanodegree/2_UIKit_Fundamentals/ColorMaker/ColorMaker/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ColorMaker
+//
+//  Created by Jason Schatz on 11/2/14.
+//  Copyright (c) 2014 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redControl: UISwitch!
+    @IBOutlet weak var greenControl: UISwitch!
+    @IBOutlet weak var blueControl: UISwitch!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set colorView on launch
+        changeColorComponent()
+    }
+    
+    @IBAction func changeColorComponent() {
+        
+        // Make sure the program doesn't crash if the controls aren't connected
+        if self.redControl == nil || self.blueControl == nil || self.blueControl == nil {
+            return
+        }
+        
+        let r: CGFloat = self.redControl.isOn ? 1 : 0
+        let g: CGFloat = self.greenControl.isOn ? 1 : 0
+        let b: CGFloat = self.blueControl.isOn ? 1 : 0
+                
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+}
+
